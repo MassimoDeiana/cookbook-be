6bad39f03570176c81ed9eb091639c75148fd9ea
Massimo Deiana <massimodeiana15@gmail.com>
2024-10-12 13:44:10 +0200

    setup

---
+++ app/src/main/kotlin/com/massimo/cookbookbe/CookbookBeApplication.kt
@@ -0,0 +1,11 @@
+package com.massimo.cookbookbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CookbookBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CookbookBeApplication>(*args)
+}
---
+++ app/src/test/kotlin/com/massimo/cookbookbe/CookbookBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.massimo.cookbookbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CookbookBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
