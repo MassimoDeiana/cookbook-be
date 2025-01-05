8203477ae9ae1a15ca3a79d0d91d342e0b15527a
Massimo Deiana <massimodeiana15@gmail.com>
2025-01-05 11:41:48 +0100

    use response entity as return type of controller method

--- api/src/main/kotlin/com/massimo/cookbookbe/controller/IngredientController.kt
+++ api/src/main/kotlin/com/massimo/cookbookbe/controller/IngredientController.kt
@@ -3,59 +3,65 @@ package com.massimo.cookbookbe.controller
 import com.massimo.cookbookbe.command.ingredient.CreateIngredientCommand
 import com.massimo.cookbookbe.command.ingredient.IngredientCommands
 import com.massimo.cookbookbe.command.ingredient.UpdateIngredientInfoCommand
+import com.massimo.cookbookbe.domain.Ingredient
 import com.massimo.cookbookbe.queries.ingredient.IngredientFilter
 import com.massimo.cookbookbe.queries.ingredient.IngredientQueries
-import com.massimo.cookbookbe.service.ImageAssistant
-//import com.massimo.cookbookbe.service.Translator
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin(origins = ["http://localhost:8081"])
 class IngredientController(
     private val ingredientQueries: IngredientQueries,
     private val ingredientCommands: IngredientCommands,
-    private val imageAssistant: ImageAssistant
-//    private val translator: Translator
 ) {
 
-    @GetMapping("/image/{imagePath}")
-    fun describeImage(@PathVariable imagePath: String) =
-        imageAssistant.describeImage(imagePath)
-
     @GetMapping("/ingredients")
-    fun findAll(@ModelAttribute ingredientFilter: IngredientFilter) =
-        ingredientQueries.findAll(ingredientFilter)
+    fun findAll(
+        @ModelAttribute ingredientFilter: IngredientFilter
+    ): ResponseEntity<List<Ingredient>> {
+
+        val ingredients = ingredientQueries.findAll(ingredientFilter)
+
+        return ResponseEntity.ok(ingredients)
+    }
 
     @GetMapping("/ingredient/{id}")
-    fun findById(@PathVariable id: Long, @RequestParam("lang") lang: String) {
+    fun findById(
+        @PathVariable id: Long
+    ) : ResponseEntity<Ingredient> {
+
         val ingredient = ingredientQueries.findById(id)
-//        ingredient.let {
-//            println(translator.translate(it.name))
-//            println(translator.translate(it.description))
-//            println(translator.translate(it.unit.name))
-//            println(translator.translate(it.category.name))
-//        }
+        return ResponseEntity.ok(ingredient)
     }
 
     @PostMapping("/ingredient")
-    fun createIngredient(@RequestBody createIngredientCommand: CreateIngredientCommand) =
-        ingredientCommands.handle(createIngredientCommand)
+    fun createIngredient(
+        @RequestBody createIngredientCommand: CreateIngredientCommand
+    ) : ResponseEntity<Long> {
+
+        val ingredientId = ingredientCommands.handle(createIngredientCommand)
+        return ResponseEntity(ingredientId, HttpStatus.CREATED)
+    }
 
     @PutMapping("/ingredient/{id}")
-    fun updateIngredient(@PathVariable id: Long, @RequestBody updateIngredientInfoCommand: UpdateIngredientInfoCommand) =
+    fun updateIngredient(
+        @PathVariable id: Long,
+        @RequestBody updateIngredientInfoCommand: UpdateIngredientInfoCommand
+    ) : ResponseEntity<Long> {
+
         ingredientCommands.handle(id, updateIngredientInfoCommand)
+        return ResponseEntity.ok().build()
+    }
 
     @DeleteMapping("/ingredient/{id}")
-    fun deleteIngredient(@PathVariable id: Long) =
+    fun deleteIngredient(
+        @PathVariable id: Long
+    ) : ResponseEntity<Unit> {
+
         ingredientCommands.handle(id)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
+    }
 
 }
--- app/src/main/kotlin/com/massimo/cookbookbe/CookbookBeApplication.kt
+++ app/src/main/kotlin/com/massimo/cookbookbe/CookbookBeApplication.kt
@@ -1,6 +1,5 @@
 package com.massimo.cookbookbe
 
-import org.jetbrains.exposed.sql.SchemaUtils
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
